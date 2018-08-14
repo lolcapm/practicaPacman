74bb9227bafb2f9aaff29452d195ed667d5ffafb
lolcapm <amayberc@gmail.com>
2018-08-14 19:27:58 -0400

    PracticaPacman

---
+++ mapaPacman/Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+    UnityEngine.AI.NavMeshAgent agenteDENavegacion;
+    public GameObject destino;
+    //private UnityEngine.AI.NavMeshAgent agenteDENavegacion;
+	void Start () {
+        agenteDENavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agenteDENavegacion.SetDestination(destino.transform.position);
+	}
+}
